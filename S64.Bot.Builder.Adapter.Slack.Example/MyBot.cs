81441ff38deff61e475a09f297c8ca1afc9126e7
Shuma Yoshioka <s64.stdio@gmail.com>
2019-07-07 20:24:15 +0900

    Create example

---
+++ S64.Bot.Builder.Adapter.Slack.Example/MyBot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace S64.Bot.Builder.Adapter.Slack.Example
+{
+
+    public class MyBot : ActivityHandler
+    {
+
+        protected override async Task OnMessageActivityAsync(
+            ITurnContext<IMessageActivity> turnContext,
+            CancellationToken cancellationToken
+        )
+        {
+            await turnContext.SendActivityAsync(
+                MessageFactory.Text($"Hello World! Your message is: `{turnContext.Activity.Text}`."),
+                cancellationToken
+            );
+        }
+
+    }
+
+}
---
+++ S64.Bot.Builder.Adapter.Slack.Example/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace S64.Bot.Builder.Adapter.Slack.Example
+{
+
+    class Program
+    {
+
+        static string MySlackBotToken
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("YOUR_SLACK_BOT_TOKEN");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var adapter = new SlackAdapter(MySlackBotToken);
+
+            var myBot = new MyBot();
+
+            adapter.ProcessActivityAsync(async (turnContext, cancellationToken) =>
+            {
+                await myBot.OnTurnAsync(turnContext, cancellationToken);
+            }).Wait();
+        }
+
+    }
+
+}

a8064788b940db773a4c67ba80b82fa7a949c12c
Shuma Yoshioka <s64.stdio@gmail.com>
2019-07-09 20:53:09 +0900

    Create options

---
+++ S64.Bot.Builder.Adapters.Slack.AspNetCore/SlackBotOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Bot.Builder;
+
+namespace S64.Bot.Builder.Adapters.Slack.AspNetCore
+{
+
+    public class SlackBotOptions
+    {
+
+        public SlackOptions SlackOptions { get; set; }
+
+        public IList<IMiddleware> Middleware { get; set; }
+
+        public SlackBotPaths Paths { get; set; }
+
+        public bool IsValid => this.Paths != null && this.Middleware != null && this.SlackOptions != null;
+
+    }
+
+}
